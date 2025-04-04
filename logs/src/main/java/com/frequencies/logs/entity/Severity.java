0578fe88717db719044baeeefe9b6f5d62cd3e0a
coredump <bigfoot_arch@proton.me>
2025-04-04 18:34:21 +0200

    wip

--- logs/src/main/java/com/frequencies/logs/entity/Severity.java
+++
@@ -1,9 +0,0 @@
-package com.frequencies.logs.entity;
-
-public enum Severity {
-    ERROR,
-    WARN,
-    INFO,
-    DEBUG,
-    TRACE
-}
--- server/src/main/java/com/frequencies/server/entity/PaymentType.java
+++
@@ -1,6 +0,0 @@
-package com.frequencies.server.entity;
-
-public enum PaymentType {
-    Nature,
-    WireTransfer
-}
